e96fe0db61b401e3a50b27f68e96bfe7f4a94c8d
calebstudios <41604604+calebstudios@users.noreply.github.com>
2023-09-23 18:01:14 -0400

    Implemented initial data loading, filtered, and aggregated for energy datasets

---
+++ EnergyDataAggregation.R
@@ -0,0 +1,30 @@
+# Load the tidyverse package and required libraries
+library(tidyverse)
+
+# Read the CSV data sets into data frames
+KAG_energydata_complete <- read_csv(
+  "Energy Usage Data/KAG_energydata_complete.csv")
+liander_2019 <- read_csv("Energy Usage Data/liander_electricity_01012019.csv")
+liander_2020 <- read_csv("Energy Usage Data/liander_electricity_01012020.csv")
+AEP_hourly <- read_csv("Energy Usage Data/AEP_hourly.csv")
+PJME_hourly <- read_csv("Energy Usage Data/PJME_hourly.csv")
+PJMW_hourly <- read_csv("Energy Usage Data/PJMW_hourly.csv")
+all_energy_statistics <- read_csv("Energy Usage Data/all_energy_statistics.csv")
+world_energy_consumption <- read_csv(
+  "Energy Usage Data/World Energy Consumption.csv")
+
+# Filter relevant columns
+KAG_energydata_filtered <- KAG_energydata_complete %>% 
+  rename(Date = date) %>% 
+  mutate(EnergyConsumption = Appliances + lights) %>% 
+  select(Date, EnergyConsumption)
+
+# Aggregate relevant columns
+liander_2019_aggregated <- liander_2019 %>% 
+  group_by(city, zipcode_from, zipcode_to) %>% 
+  summarise(TotalAnnualConsume = sum(annual_consume), 
+            AvgSmartMeterPerc = mean(smartmeter_perc))
+liander_2020_aggregate <- liander_2020 %>% 
+  group_by(city, zipcode_from, zipcode_to) %>% 
+  summarise(TotalAnnualConsume = sum(annual_consume), 
+            AvgSmartMeterPerc = mean(smartmeter_perc))
